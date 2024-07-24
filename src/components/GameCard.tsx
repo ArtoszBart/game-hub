5f16aebeacaba1a8c6477b2342e02d8611e8c42b
Bartosz Art <bartosz.art@gmail.com>
2024-07-24 12:29:49 +0200

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
 import { IGame } from '../hooks/useGames';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
+import getCroppedImageUrl from '../services/imageUrl';
 
 interface IProps {
 	game: IGame;
@@ -10,7 +11,7 @@ interface IProps {
 const GameCard = ({ game }: IProps) => {
 	return (
 		<Card borderRadius={10} overflow='hidden'>
-			<Image src={game.background_image} />
+			<Image src={getCroppedImageUrl(game.background_image)} />
 			<CardBody>
 				<Heading fontSize='2xl'>{game.name}</Heading>
 				<HStack justifyContent='space-between'>
