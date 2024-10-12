402d101c15caaa3684b58d7a6e2552264fc26afb
Bartosz Art <bartosz.art@gmail.com>
2024-10-12 14:21:17 +0200

    Build game attributes

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,35 @@
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import { IGame } from '../models/IGame';
+import CriticScore from './CriticScore';
+import DefinitionItem from './DefinitionItem';
+
+interface IProps {
+	game: IGame;
+}
+
+const GameAttributes = ({ game }: IProps) => {
+	return (
+		<SimpleGrid columns={2}>
+			<DefinitionItem term='Platforms'>
+				{game.parent_platforms?.map(({ platform }) => (
+					<Text key={platform.id}>{platform.name}</Text>
+				))}
+			</DefinitionItem>
+			<DefinitionItem term='Metascore'>
+				<CriticScore score={game.metacritic} />
+			</DefinitionItem>
+			<DefinitionItem term='Genres'>
+				{game.genres?.map((genre) => (
+					<Text key={genre.id}>{genre.name}</Text>
+				))}
+			</DefinitionItem>
+			<DefinitionItem term='Publishers'>
+				{game.publishers?.map((publisher) => (
+					<Text key={publisher.id}>{publisher.name}</Text>
+				))}
+			</DefinitionItem>
+		</SimpleGrid>
+	);
+};
+
+export default GameAttributes;
--- src/models/IGame.ts
+++ src/models/IGame.ts
@@ -1,9 +1,12 @@
 import { IPlatform } from './IPlatform';
+import { IPublisher } from './IPublisher';
 
 export interface IGame {
 	id: number;
 	name: string;
 	slug: string;
+	genres: IGame[];
+	publishers: IPublisher[];
 	description_raw: string;
 	background_image: string;
 	parent_platforms: { platform: IPlatform }[];
