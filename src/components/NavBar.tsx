9c8649e2cd7669244e2af8cfdb8c23f0aaa37773
Bartosz Art <bartosz.art@gmail.com>
2024-11-23 17:24:43 +0100

    New logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,4 @@
 import { HStack, Image } from '@chakra-ui/react';
-import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
 import { Link } from 'react-router-dom';
@@ -8,7 +7,7 @@ const NavBar = () => {
 	return (
 		<HStack padding={'10px'}>
 			<Link to='/'>
-				<Image src={logo} boxSize={'60px'} objectFit='cover' />
+				<Image src='/game-hub-logo.svg' boxSize={'60px'} />
 			</Link>
 			<SearchInput />
 			<ColorModeSwitch />
